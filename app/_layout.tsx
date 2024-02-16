23e6ffc2f5004db8ee3cdd9b8b68c1786227cbf0
Andras Szesztai <and.szesztai@gmail.com>
2024-02-16 11:34:26 +0100

    chore: init tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,67 +1,27 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome'
-import {
-    DarkTheme,
-    DefaultTheme,
-    ThemeProvider,
-} from '@react-navigation/native'
-import { useFonts } from 'expo-font'
-import { Stack } from 'expo-router'
-import * as SplashScreen from 'expo-splash-screen'
-import { useEffect } from 'react'
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { TamaguiProvider } from 'tamagui';
 
-import { useColorScheme } from '@/components/useColorScheme'
+import config from '../tamagui.config';
 
-export {
-    // Catch any errors thrown by the Layout component.
-    ErrorBoundary,
-} from 'expo-router'
+export default function Layout() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
 
-export const unstable_settings = {
-    // Ensure that reloading on `/modal` keeps a back button present.
-    initialRouteName: '(tabs)',
-}
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync()
-
-export default function RootLayout() {
-    const [loaded, error] = useFonts({
-        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-        ...FontAwesome.font,
-    })
-
-    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-    useEffect(() => {
-        if (error) throw error
-    }, [error])
-
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync()
-        }
-    }, [loaded])
-
-    if (!loaded) {
-        return null
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
+  }, [loaded]);
 
-    return <RootLayoutNav />
-}
+  if (!loaded) return null;
 
-function RootLayoutNav() {
-    const colorScheme = useColorScheme()
-
-    return (
-        <ThemeProvider
-            value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        >
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen
-                    name="modal"
-                    options={{ presentation: 'modal' }}
-                />
-            </Stack>
-        </ThemeProvider>
-    )
+  return (
+    <TamaguiProvider config={config}>
+      <Stack />
+    </TamaguiProvider>
+  );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function (api) {
-    api.cache(true)
-    return {
-        presets: ['babel-preset-expo'],
-    }
-}
+  api.cache(true);
+  const plugins = [];
+
+  plugins.push([
+    '@tamagui/babel-plugin',
+    {
+      components: ['tamagui'],
+      config: './tamagui.config.ts',
+    },
+  ]);
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins,
+  };
+};
