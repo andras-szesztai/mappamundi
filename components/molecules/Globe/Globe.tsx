51c35c323abd40f9a1eaa979f9cfa55802d80304
Andras Szesztai <and.szesztai@gmail.com>
2024-02-24 16:19:46 +0100

    feat(map-16): country selection setup

--- components/molecules/Globe/Globe.tsx
+++ components/molecules/Globe/Globe.tsx
@@ -1,7 +1,9 @@
 import Mapbox, { Camera } from '@rnmapbox/maps';
+import { useState } from 'react';
 import { View, useWindowDimensions } from 'react-native';
 
 import globeData from '~/assets/countries.json';
+import Colors from '~/constants/Colors';
 
 Mapbox.setAccessToken(
   'pk.eyJ1Ijoic3plYW5kciIsImEiOiJjbHN5eTI4b2gwa2JjMmtwMmNtOWtjcWx6In0.Ae7WTlD4Heg2XoOdQEcP4g'
@@ -9,16 +11,20 @@ Mapbox.setAccessToken(
 
 export function Globe() {
   const { height, width } = useWindowDimensions();
+  const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
+  const [guessedCountry, setGuessedCountry] = useState<string | null>(null);
+  const selectedShape = globeData.features.find(
+    (feature: any) => feature.properties.formal_en === selectedCountry
+  );
+  const guessedShape = guessedCountry && selectedShape;
   return (
     <View
-      className="flex bg-verdigris"
       style={{
         width,
         height,
       }}>
       <Mapbox.MapView
         projection="globe"
-        className="flex-1"
         styleURL="mapbox://styles/szeandr/clsz16wfp00ej01megfms087d"
         rotateEnabled={false}
         logoEnabled={false}
@@ -29,19 +35,47 @@ export function Globe() {
         }}>
         <Camera maxZoomLevel={10} minZoomLevel={1} />
         <Mapbox.ShapeSource
+          id="countrySource"
           onPress={(e) => {
-            console.log(e.features);
+            setGuessedCountry(null);
+            setSelectedCountry(e.features[0].properties?.formal_en);
           }}
-          id="smileyFaceSource"
           shape={globeData as any}>
           <Mapbox.FillLayer
             id="countriesLayer"
             style={{
               fillColor: 'transparent',
-              fillOutlineColor: 'black',
+              fillOutlineColor: Colors.raisinBlack,
             }}
           />
         </Mapbox.ShapeSource>
+        {selectedShape && (
+          <Mapbox.ShapeSource
+            id="selectedCountrySource"
+            shape={selectedShape as any}
+            onPress={(e) => {
+              setGuessedCountry(e.features[0].properties?.formal_en);
+            }}>
+            <Mapbox.FillLayer
+              id="selectedCountryLayer"
+              style={{
+                fillColor: Colors.verdigris,
+                fillOutlineColor: Colors.midnightGreen,
+              }}
+            />
+          </Mapbox.ShapeSource>
+        )}
+        {guessedShape && (
+          <Mapbox.ShapeSource id="guessedCountrySource" shape={guessedShape as any}>
+            <Mapbox.FillLayer
+              id="guessedCountryLayer"
+              style={{
+                fillColor: Colors.midnightGreen,
+                fillOutlineColor: Colors.midnightGreen,
+              }}
+            />
+          </Mapbox.ShapeSource>
+        )}
       </Mapbox.MapView>
     </View>
   );
